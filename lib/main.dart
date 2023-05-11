d0322c4aa2162c017b1e891a013adb8dca2e5a2f
ROCIO_ALEJANDRA_HURTADO_HEREDIA <a.20308051281009@cbtis128.edu.mx>
2023-05-11 16:08:57 +0000

    Version1

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,20 +24,76 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          title: const Text('Carls Jr'),
+          centerTitle: true,
+          backgroundColor: const Color(0xff000000),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_vert,
+              ),
+              onPressed: () {},
+            )
+          ],
         ),
-      ),
-    );
+        body: Container(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                Text("Ingresa tus datos:"),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "ID",
+                      labelText: "Escribe tu id",
+                      icon: Icon(Icons.ad_units_rounded)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Nombre",
+                      labelText: "Escribe tu nombre",
+                      icon: Icon(Icons.account_circle_rounded)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Apellido",
+                      labelText: "Escribe tu apellido",
+                      icon: Icon(Icons.account_circle_rounded)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Telefono",
+                      labelText: "Escribe tu numero de telefono",
+                      icon: Icon(Icons.call)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Direccion",
+                      labelText: "Escribe tu direccion",
+                      icon: Icon(Icons.location_on)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Sucursal",
+                      labelText: "Escribe tu sucursal",
+                      icon: Icon(Icons.location_city)),
+                ),
+                TextField(
+                    decoration: InputDecoration(
+                        hintText: "Contraseña",
+                        labelText: "Escribe tu contraseña",
+                        icon: Icon(Icons.lock)),
+                    obscureText: true),
+              ],
+            )));
   }
 }
